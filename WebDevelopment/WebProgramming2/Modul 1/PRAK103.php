8adfca8f318c1c77f96b2d586a398f5a474b2b2b
Minamoto Yuki <69187285+MinamotoYuki46@users.noreply.github.com>
2025-04-02 10:57:02 +0800

    minor update

--- WebDevelopment/WebProgramming2/Modul 1/PRAK103.php
+++ WebDevelopment/WebProgramming2/Modul 1/PRAK103.php
@@ -6,7 +6,7 @@
     $kelvin = $celcius + 273.15;
 
     echo "
-        Celcius: " . number_format($celcius, 4, ',') . "<br>" + 
+        Celcius: " . number_format($celcius, 3, ',') . "<br>" + 
         "Fahrenheit: " . number_format($fahrenheit, 4, ',') . "<br>" +
         "Reamur: " . number_format($reamur, 4, ',') . "<br>" +
         "Kelvin: " . number_format($kelvin, 4, ',') . "<br>"
